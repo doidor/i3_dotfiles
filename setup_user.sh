bf910f0a1a3c2d20dcdcb61264d410babfe6f2bc
doidor <tudor@blacktree.software>
2018-10-29 10:44:53 +0200

    added zshrc file into dotflies folder

--- setup_user.sh
+++ setup_user.sh
@@ -5,6 +5,7 @@ wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - 
 chsh -s `which zsh`
 
 mkdir -p ~/.config/i3status/
-cp ./dotfiles/i3status_config ~/.config/i3status/config
 
+cp ./dotfiles/i3status_config ~/.config/i3status/config
 cp ./dotfiles/i3_config ~/.config/i3/config
+cp ./dotfiles/zshrc ~/.zshrc
