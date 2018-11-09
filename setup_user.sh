12c5ca67f425578960e5df8c8a3fbc088ae06e31
doidor <tdr.popa@gmail.com>
2018-11-09 23:07:00 +0200

    added a way to do screenshots

--- setup_user.sh
+++ setup_user.sh
@@ -5,9 +5,12 @@ wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - 
 chsh -s `which zsh`
 
 mkdir -p ~/.config/i3status/
+mkdir -p ~/Pictures
 
 cp ./dotfiles/i3status_config ~/.config/i3status/config
 cp ./dotfiles/i3_config ~/.config/i3/config
 cp ./dotfiles/zshrc ~/.zshrc
 
-wget -O gogh https://git.io/vQgMr && chmod +x gogh && ./gogh && rm gogh
+# Screen Shots
+bindsym Print exec scrot '%Y-%m-%d-%s_screenshot_$wx$h.jpg' -e 'mv $f ~/Pictures/'
+bindsym $mod+Print exec gnome-screenshot -i
