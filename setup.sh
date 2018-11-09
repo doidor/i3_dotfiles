0ec009e1b07a1ae5de0502485f56cd7428f7897f
doidor <tdr.popa@gmail.com>
2018-11-09 23:12:46 +0200

    added a way to do screenshots

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+DEBS_FOLDER=/tmp/debs
+
 add-apt-repository main
 add-apt-repository universe
 add-apt-repository restricted
@@ -9,16 +11,14 @@ apt update
 
 apt install -y lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings i3 wicd ubuntu-drivers-common mesa-utils mesa-utils-extra compton xorg xserver-xorg nautilus gnome-terminal git git-core zsh fonts-powerline xfe xfe-i18n xfe-themes arandr dconf-cli feh gnome-screenshot scrot
 
-mkdir /tmp/debs/
+mkdir $DEBS_FOLDER
 
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /tmp/debs/chrome.deb
-wget https://go.microsoft.com/fwlink/?LinkID=760868 -O /tmp/debs/visualstudiocode.deb
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O $DEBS_FOLDER/chrome.deb
+wget https://go.microsoft.com/fwlink/?LinkID=760868 -O $DEBS_FOLDER/visualstudiocode.deb
+wget https://downloads.slack-edge.com/linux_releases/slack-desktop-3.3.3-amd64.deb -O $DEBS_FOLDER/slack.deb
+wget https://repo.skype.com/latest/skypeforlinux-64.deb -O $DEBS_FOLDER/skype.deb
 
-dpkg -i /tmp/debs/*.deb
+dpkg -i $DEBS_FOLDER/*.deb
 apt install -f -y
-
-wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
-
-chsh -s `which zsh`
 
 poweroff --reboot
--- setup_user.sh
+++ setup_user.sh
@@ -6,11 +6,8 @@ chsh -s `which zsh`
 
 mkdir -p ~/.config/i3status/
 mkdir -p ~/Pictures
+mkdir -p ~/wallpapers
 
 cp ./dotfiles/i3status_config ~/.config/i3status/config
 cp ./dotfiles/i3_config ~/.config/i3/config
 cp ./dotfiles/zshrc ~/.zshrc
-
-# Screen Shots
-bindsym Print exec scrot '%Y-%m-%d-%s_screenshot_$wx$h.jpg' -e 'mv $f ~/Pictures/'
-bindsym $mod+Print exec gnome-screenshot -i
