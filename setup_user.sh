3d829210333d1d880d73994caea5f39d7167fa73
doidor <tdr.popa@gmail.com>
2019-05-31 21:11:16 +0300

    added terminator and nerd-fonts

--- setup_user.sh
+++ setup_user.sh
@@ -4,6 +4,12 @@ wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - 
 
 chsh -s `which zsh`
 
+# setup nerd fonts and Hermit
+git clone https://github.com/ryanoasis/nerd-fonts.git ~/nerd-fonts
+pushd ~/nerd-fonts
+./install Hermit
+popd
+
 mkdir -p ~/.config/i3status/
 mkdir -p ~/Pictures
 mkdir -p ~/wallpapers
